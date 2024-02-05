3ac1a99926b51612b59f615abea4ad5dcac3211c
credibleforce <jamiefrasermcmurray@gmail.com>
2024-02-05 13:17:59 -0800

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t hydra-static .
-docker run --rm --entrypoint=/bin/sh -v "${PWD}/binaries/linux/x86_64:/binaries" -v "${PWD}/wordlists:/wordlists" hydra-static -c 'cp $(which hydra) /binaries/hydra && cp -pR /opt/usernames /opt/passwords /wordlists'
+docker run --rm --entrypoint=/bin/sh -v "${PWD}/binaries/linux/x86_64:/binaries" -v "${PWD}/wordlists:/wordlists" hydra-static -c 'cp $(which hydra) /binaries/hydra && tar -zcvf /opt/password /opt/usernames /wordlists/wordlists.tgz'
